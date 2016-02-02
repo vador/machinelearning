c84342082102075fb14bddfd6f06a3ed6b5ba5a5
vador <vador@asherah.dador.org>
2016-02-02 13:46:11 +0100

    Gradient descend + submission ok

--- machine-learning-ex2/ex2/costFunction.m
+++ machine-learning-ex2/ex2/costFunction.m
@@ -22,7 +22,7 @@ grad = zeros(size(theta));
 
 h_theta = sigmoid(X*theta);
 
-J = -1/m*(y*log(h_theta'))*((1-y)*log(1-h_theta')) ;
+J = -1/m*((y'*log(h_theta))+((1-y)'*log(1-h_theta))) ;
 
 grad = 1/m * X' *(h_theta - y)
 
