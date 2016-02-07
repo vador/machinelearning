ac82d929b560ad81ef4bcffbe24b1b41a7b57d78
vador <vador@asherah.dador.org>
2016-02-07 11:29:21 +0100

    Passed learning but variance in gradient

--- machine-learning-ex4/ex4/nnCostFunction.m
+++ machine-learning-ex4/ex4/nnCostFunction.m
@@ -64,42 +64,50 @@ Theta2_grad = zeros(size(Theta2));
  % y_i = eye(num_labels)(:,y(i))
 
 Yb = eye(num_labels)(:,y) ;% Y(:,i) is y^(i) as binary vector  Yb is in columns
-% Yb size is K*m
-a1= [ones(size(X, 1), 1) X];
-z2 = (a1 * Theta1');
-a2 = [ones(size(z2, 1), 1) sigmoid(z2)];
-z3 = (a2 * Theta2');
-a3 = sigmoid(z3); % this is h_theta(x) size is m*K
+% Yb size is K*m 10*5000
+a1= [ones(size(X, 1), 1) X];  % 5000 * 401 
+z2 = (a1 * Theta1'); % 5000 * 25
+a2 = [ones(size(z2, 1), 1) sigmoid(z2)]; % 5000 * 26
+z3 = (a2 * Theta2'); % 5000 * 10
+a3 = sigmoid(z3); % this is h_theta(x) size is m*K 
 %[vp, p] = max(a3, [],2); % Prediction in p
 h_theta = a3 ;
-reg_factor = 0 ;
 
 %J = -1/m*((Yb*log(h_theta))+((1-Yb)*log(1-h_theta))) + reg_factor ;
 for i = 1:m
-  tmp = (log(h_theta(i,:))*(Yb(:,i))+(log(1-h_theta(i,:)))*(1-Yb(:,i))) + reg_factor ;
+  tmp = (log(h_theta(i,:))*(Yb(:,i))+(log(1-h_theta(i,:)))*(1-Yb(:,i)))  ;
   J = J+tmp ;
 end
 
 J = -1/m * J ;
 
-delta_3_full = (a3 - Yb') ; % size is m*K
+delta_3_full = (a3 - Yb') ; % size is m*K 5000*10
 
-DELTA_1 = zeros(size(Theta1)) ; % u2 * (u1+1)
-DELTA_2 = zeros(size(Theta2)) ; % K * (u2+1)
+DELTA_1 = zeros(size(Theta1)) ; % u2 * (u1+1) 25*401
+DELTA_2 = zeros(size(Theta2)) ; % K * (u2+1) 10*26
 
 for i = 1:m
-  delta_3 = delta_3_full(i,:)' ; % K*1 size
-  fprintf("delta_3 size %f, %f\n",size(delta_3,1), size(delta_3,2));
+  delta_3 = delta_3_full(i,:)' ; % K*1 size 10*1
+  %fprintf("delta_3 size %f, %f\n",size(delta_3,1), size(delta_3,2));
 
-  delta_2 = (Theta2' * delta_3)(2:end) .* sigmoidGradient(z2(i,:)'); % u_2+1 * 1 size
-  fprintf("delta_2 size %f, %f\n",size(delta_2,1), size(delta_2,2));
-  DELTA_2 = DELTA_2 + delta_3 * a2(i,:) ; % K * (u2+1)
-  DELTA_1 = DELTA_1 + (delta_2 * a1(i,:)) ; % u2 * 
+  delta_2 = (Theta2' * delta_3)(2:end) .* sigmoidGradient(z2(i,:)'); % u_2 * 1 size 25*1
+  %fprintf("delta_2 size %f, %f\n",size(delta_2,1), size(delta_2,2));
+  DELTA_2 = DELTA_2 + delta_3 * a2(i,:) ; % K * (u2) 10*26
+  DELTA_1 = DELTA_1 + (delta_2 * a1(i,:)) ; % u2 * 25*401
 end
 
-Theta1_grad = DELTA_1 ;
-Theta2_grad = DELTA_2 ;
+Theta1_grad = 1/m*DELTA_1 ;
+Theta2_grad = 1/m*DELTA_2 ;
 
+% Regularization calculation
+
+reg_fact = lambda / (2*m) * (sum(sumsq((Theta1(:,2:end) )))+ sum(sumsq(Theta2(:,2:end)))) ;
+J = J + reg_fact ;
+
+reg_grad1 = [zeros(size(Theta1,1),1) lambda/m*Theta1(:,2:end)] ;
+reg_grad2 = [zeros(size(Theta2,1),1) lambda/m*Theta2(:,2:end)] ;
+Theta1_grad = Theta1_grad + reg_grad1 ;
+Theta2_grad = Theta2_grad + reg_grad2 ;
 % -------------------------------------------------------------
 
 % =========================================================================
