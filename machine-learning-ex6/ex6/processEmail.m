fb9bd1d4d4caa5c3d59cba6f203951101c6cc366
Damien Salvador <vador@grabeuh.com>
2016-02-15 14:37:05 +0100

    spam detection

--- machine-learning-ex6/ex6/processEmail.m
+++ machine-learning-ex6/ex6/processEmail.m
@@ -96,15 +96,13 @@ while ~isempty(email_contents)
     % Note: You can use strcmp(str1, str2) to compare two strings (str1 and
     %       str2). It will return 1 only if the two strings are equivalent.
     %
-
-
-
-
-
-
-
-
-
+    
+    idx = find(ismember(vocabList, str)) ;
+    
+    %fprintf('%f ' , idx) ;
+    if length(idx) >0
+      word_indices(end+1) = idx ;
+    end
 
     % =============================================================
 
